938bba3b6a14c8b5e55e4a6d9f68b0fbac1647e5
Maula Syabil Haqq <shirushi@student.telkomuniversity.ac.id>
2022-02-23 21:56:24 +0700

    Fix Bug Player Movement Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController characterController;
+    private Animator animator;
+
+    [SerializeField]
+    private float moveSpeed = 100;
+    [SerializeField]
+    private float turnSpeed = 5f;
+    private void Awake() 
     {
-        
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+     private void Update()
     {
-        
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(horizontal, 0, vertical);
+
+        characterController.SimpleMove(movement * Time.deltaTime * moveSpeed); 
+
+        animator.SetFloat("Speed", movement.magnitude);
+
+
+        if (movement.magnitude > 0)
+        {
+        Quaternion newDirection = Quaternion.LookRotation(movement);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, newDirection, Time.deltaTime * turnSpeed);
+        }
     }
 }
